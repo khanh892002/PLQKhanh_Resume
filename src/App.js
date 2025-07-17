28a7286353e8e4d5b5c27eb0c8b4095bdca2e35d
Khanh8902 <116865538+khanh892002@users.noreply.github.com>
2025-07-18 00:52:57 +0700

    Now I can tell for sure that App is not at fault for not rendering root div. Since the screen only have Loading..., I figure the problem is at fetched cv data, still null after running useEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { PersonalInfo } from './Components/PersonalInfo';
 import { Education } from './Components/Education';
@@ -8,9 +9,14 @@ import { Skills } from './Components/Skills';
 import { LanguageSkill } from './Components/LanguageSkill';
 
 function App() {
-  const reqCV = fetch('./cv.json');
-  const cv = reqCV.json();
-  return (
+  const [cv, setCV] = useState(null);
+  useEffect(() => {
+    fetch("/cv.json")
+    .then((res) => res.json())
+    .then((json) => setCV(json))
+    .catch((err) => console.error("Error: ", err))
+  }, []);
+  return (cv) ? (
     <div className="App">
       <PersonalInfo data={cv.PersonalInfo}/>
       <Education data={cv.Education}/>
@@ -20,7 +26,7 @@ function App() {
       <Skills data={cv.Skills}/>
       <LanguageSkill data={cv.LangSkills}/>
     </div>
-  );
+  ) : <div>Loading...</div>;
 }
 
 export default App;
